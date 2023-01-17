8bd6f9b0fe1b0053a89e6b826ca433a03dd38033
Connor McGwire <cdmcgwire@users.noreply.github.com>
2023-01-17 13:52:40 -0700

    Established preferred project format as of Jan23

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<a href="sub">Sub page</a>
+
+<style lang="sass">
+	h1
+		color: design.$color-fg
+		background: design.$color-bg
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,53 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-static'
+import sveltePreprocess from 'svelte-preprocess'
+import { mdsvex } from 'mdsvex'
+import rehypeSlug from 'rehype-slug'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import rehypeAddClasses from 'rehype-add-classes'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
+	extensions: ['.svelte', '.md'],
+	preprocess: [
+		sveltePreprocess({
+			sourceMap: true,
+			babel: {
+				presets: [
+					[
+						'@babel/preset-env',
+						{
+							loose: true,
+							modules: false,
+							targets: {
+								esmodules: true
+							}
+						}
+					]
+				]
+			},
+			sass: {
+				prependData: `@use 'src/lib/styles/_design.sass'`,
+				outputStyle: 'compressed'
+			}
+		}),
+		mdsvex({
+			extensions: ['.md'],
+			rehypePlugins: [
+				[
+					rehypeAddClasses,
+					{
+						'h1,h2,h3,pre,blockquote,p,ul,li,ol,strong,em,hr': 'markdown'
+					}
+				],
+				rehypeSlug,
+				rehypeAutolinkHeadings
+			]
+		})
+	],
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		appDir: 'internal'
 	}
-};
+}
 
-export default config;
+export default config
